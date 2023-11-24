9952716044da7bec2bddd3caca3f96ee520b66e0
kastastin <kostya242421@gmail.com>
2023-11-24 18:31:49 +0200

    Data Structures

---
+++ studyJS/dataStructures/destructuring.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// const arr = [5, 10, 15];
+// let [x, y, z] = arr;
+
+// const temp = x;
+// x = y;
+// y = temp;
+// [x, y] = [y, x];
+
+// const days = ['mon', 'tue'];
+// const info = {
+//   [days[0]]: {
+//     open: 9,
+//     close: 20,
+//   },
+//   [days[1]]: {
+//     open: 10,
+//     close: 22,
+//   },
+//   [`day-${4 + 2}`]: {
+//     open: 12,
+//     close: 18,
+//   },
+// };
+// console.log(info['day-6']);
+
+// const str = 'blue and red colors without red';
+// console.log(str.replace(/red/g, 'white'));
