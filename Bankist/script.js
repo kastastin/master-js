8686560ee48ce2dee9ec28b153f636c985ae6e6f
kastastin <kostya242421@gmail.com>
2023-11-30 13:53:02 +0200

    Bankist implemented sorting

--- Bankist/script.js
+++ Bankist/script.js
@@ -28,9 +28,22 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // <-- Data -->
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -38,23 +51,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -64,7 +76,8 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-let currAccount;
+let currAccount,
+  isMovementsSorted = false;
 createUsernames(accounts);
 
 // <-- Event handlers -->
@@ -143,6 +156,12 @@ document.addEventListener('click', (e) => {
       inputClosePin.blur();
     }
   }
+
+  // Sort movements event
+  if (clickedElClass.includes('btn--sort')) {
+    isMovementsSorted = !isMovementsSorted;
+    displayMovements(currAccount.movements, isMovementsSorted);
+  }
 });
 
 function createUsernames(accounts) {
@@ -182,14 +201,14 @@ function calcDisplaySummary(acc) {
   labelSumInterest.textContent = `${interest}€`;
 }
 
-function getSum(arr) {
-  return arr.reduce((acc, curr) => acc + curr);
-}
-
-function displayMovements(movements) {
+function displayMovements(movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach((val, i) => {
+  const sortedMovements = sort
+    ? movements.slice().sort((a, b) => a - b)
+    : movements;
+
+  sortedMovements.forEach((val, i) => {
     const type = val > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -203,4 +222,8 @@ function displayMovements(movements) {
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
+}
+
+function getSum(arr) {
+  return arr.reduce((acc, curr) => acc + curr);
 }
