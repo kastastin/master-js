a9bdf86ad8c2ec748c65ebcf8545f3a8879192aa
kastastin <kostya242421@gmail.com>
2023-12-04 01:25:45 +0200

    Bankist Landing Event propagation

--- Bankist-Landing/script.js
+++ Bankist-Landing/script.js
@@ -21,6 +21,7 @@ const header = document.querySelector('.header'),
   message = document.createElement('div');
 
 message.classList.add('cookie-message');
+message.style.backgroundColor = '#37383d';
 message.innerHTML =
   'We use cookies for impoved functionality and analytics.<button class="btn btn--close-cookie" > Got it!</ > ';
 header.prepend(message);
@@ -28,3 +29,25 @@ header.prepend(message);
 document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   message.parentElement.removeChild(message);
 });
+
+// <-- Smooth scrolling -->
+const scrollToBtn = document.querySelector('.btn--scroll-to'),
+  section1 = document.querySelector('#section--1');
+
+scrollToBtn.addEventListener('click', () => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// <-- Event propagation -->
+console.log(getRndColor());
+
+function getRndColor() {
+  const [r, g, b] = Array.from({ length: 3 }, () => getRndInt(0, 255));
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function getRndInt(min, max) {
+  let rnd = min + Math.random() * (max + 1 - min);
+  return Math.floor(rnd);
+}
