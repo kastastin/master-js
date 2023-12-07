a9ac05bc4f11fd882b477ce63a0a2e9f2a538eba
kastastin <kostya242421@gmail.com>
2023-12-07 15:10:52 +0200

    OOP Prototypes

--- studyJS/fundamentals/OOP/script.js
+++ studyJS/fundamentals/OOP/script.js
@@ -1,14 +1,40 @@
 'use strict';
 
-// <-- Constructor functions and the new operator -->
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
+// <-- Prototypes -->
+prototypes();
+function prototypes() {
+  const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  };
 
-const kastastin = new Person('Kostya', 2002);
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
-console.log(kastastin instanceof Person); // true
+  Person.prototype.species = 'Homo Sapiens';
+  Person.prototype.displayAge = function () {
+    console.log(new Date().getFullYear() - this.birthYear);
+  };
+
+  const kastastin = new Person('Kostya', 2002);
+  kastastin.displayAge();
+  console.log(kastastin.__proto__);
+  console.log(kastastin.__proto__.species); // Homo Sapiens
+  console.log(kastastin.hasOwnProperty('firstName')); // true
+  console.log(kastastin.hasOwnProperty('species')); // false
+  console.log(kastastin.__proto__ === Person.prototype); // true
+  console.log(Person.prototype.isPrototypeOf(kastastin)); // true
+}
+
+// <-- Constructor functions and the new operator -->
+// constructorFunction();
+function constructorFunction() {
+  const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  };
+
+  const kastastin = new Person('Kostya', 2002);
+  // 1. New {} is created
+  // 2. function is called, this = {}
+  // 3. {} linked to prototype
+  // 4. function automatically return {}
+  console.log(kastastin instanceof Person); // true
+}
