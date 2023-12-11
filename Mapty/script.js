4e83204f96d0b53722aee7c96a70d5cc389f71d1
kastastin <kostya242421@gmail.com>
2023-12-11 18:10:04 +0200

    Mapty Refactoring for Project Architecture

--- Mapty/script.js
+++ Mapty/script.js
@@ -11,37 +11,77 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-navigator.geolocation.getCurrentPosition(
-  (position) => {
+class App {
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => {
+        alert('Could not get your position');
+      });
+  }
+
+  _loadMap(position) {
     const { latitude: lat } = position.coords;
     const { longitude: lng } = position.coords;
     const coords = [lat, lng];
-    const map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, 13);
 
     L.tileLayer(
       'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png'
-    ).addTo(map);
+    ).addTo(this.#map);
 
-    map.on('click', (mapEvent) => {
-      const { lat, lng } = mapEvent.latlng;
-      const clickedCoords = [lat, lng];
-
-      L.marker(clickedCoords)
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('Workout')
-        .openPopup();
-    });
-  },
-  () => {
-    alert('Could not get your position');
+    // Handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
   }
-);
+
+  _showForm(e) {
+    this.#mapEvent = e;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    [inputElevation, inputCadence].forEach((el) =>
+      el.closest('.form__row').classList.toggle('form__row--hidden')
+    );
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    // Display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    const clickedCoords = [lat, lng];
+
+    L.marker(clickedCoords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
+
+const app = new App();
